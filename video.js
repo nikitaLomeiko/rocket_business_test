c4071910822bfe50b488978d6d86a4cb3a4149a7
Nikita <nikitalomieiko@gmail.com>
2025-04-20 14:25:47 +0300

    feat YT api

--- video.js
+++ video.js
@@ -1,16 +1,27 @@
+function onYouTubeIframeAPIReady() {
+    const player = new YT.Player('frame', {
+      events: {
+        'onStateChange': onPlayerStateChange 
+      }
+    });
+  }
+
+  function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+      frame.style.display = 'none'
+      buttonShowVideo.style.display = 'flex'
+      frame.src = ''
+    }
+  }
+
 const frame = document.getElementById("frame");
 const buttonShowVideo = document.getElementById("show-video");
 
 frame.style.display = "none";
 
 buttonShowVideo.addEventListener("click", () => {
+  frame.src =
+    "https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&mute=0&rel=0&controls=1&modestbranding=1&showinfo=0";
   buttonShowVideo.style.display = "none";
   frame.style.display = "block";
-
-  frame.src = 'https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&mute=0&rel=0&controls=1&modestbranding=1&showinfo=0'
-
-//   setTimeout(() => {
-//     buttonShowVideo.style.display = "flex";
-//     frame.style.display = "none";
-//   }, 7000);
 });
