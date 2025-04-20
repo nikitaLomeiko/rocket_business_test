c12aa64bebadb66aa86694aa545855673cf8e041
Nikita <nikitalomieiko@gmail.com>
2025-04-20 14:12:52 +0300

    last fix :)

--- video.js
+++ video.js
@@ -9,8 +9,8 @@ buttonShowVideo.addEventListener("click", () => {
 
   frame.src = 'https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&mute=0&rel=0&controls=1&modestbranding=1&showinfo=0'
 
-  setTimeout(() => {
-    buttonShowVideo.style.display = "flex";
-    frame.style.display = "none";
-  }, 7000);
+//   setTimeout(() => {
+//     buttonShowVideo.style.display = "flex";
+//     frame.style.display = "none";
+//   }, 7000);
 });
