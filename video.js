3ee8f5a92e9a1659f0261b030129bdd42ca08c7f
Nikita <nikitalomieiko@gmail.com>
2025-04-20 14:12:22 +0300

    fix video

--- video.js
+++ video.js
@@ -7,9 +7,10 @@ buttonShowVideo.addEventListener("click", () => {
   buttonShowVideo.style.display = "none";
   frame.style.display = "block";
 
+  frame.src = 'https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&mute=0&rel=0&controls=1&modestbranding=1&showinfo=0'
+
   setTimeout(() => {
     buttonShowVideo.style.display = "flex";
     frame.style.display = "none";
-    frame.src = 'https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&mute=0&rel=0&controls=1&modestbranding=1&showinfo=0'
   }, 7000);
 });
